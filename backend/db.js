2576b1bf7efc9cd83897050fdeec9612728272d8
Daxit godhani <dakshitgodhani103@gmail.com>
2023-02-06 10:44:29 +0530

    data transfer

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,12 @@
 const mongoose=require("mongoose")
-const mongourl="mongodb://localhost:27017/"
+const mongourl="mongodb://localhost:27017/inotebook"
 const connectToMongo=()=>{
-    mongoose.connect(mongourl,()=>{
+    mongoose.connect(mongourl,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            useCreateTndex:true
+        },
+        ()=>{
         console.log("connect success");
     })
 }
